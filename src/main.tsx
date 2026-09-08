4cb2bf89f5e8baf543bd35903889639f58d92df4
matthewcodergamer <gamerstriper@gmail.com>
2026-09-08 00:41:01 -0400

    Load QQURZ v1.8 styles

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './v1.4.css';
 import './v1.5.css';
 import './v1.6.css';
 import './v1.7.css';
+import './v1.8.css';
 import AppV14 from './AppV14';
 
 createRoot(document.getElementById('root')!).render(
