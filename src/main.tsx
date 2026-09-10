aceeff146f27b43097233c34b28058fe2d8f378b
matthewcodergamer <gamerstriper@gmail.com>
2026-09-09 23:38:22 -0400

    Load real-quarter presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import './v1.9.css';
 import './v2.0.css';
 import './v2.1.css';
 import './v2.2.css';
+import './v2.3.css';
 import AppV14 from './AppV14';
 
 createRoot(document.getElementById('root')!).render(
