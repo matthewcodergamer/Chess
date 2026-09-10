febb14864e1d0d74c5ee4c6843163e8a09b48d80
matthewcodergamer <gamerstriper@gmail.com>
2026-09-09 23:29:38 -0400

    Load QQURZ v2.2 visual system

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import './v1.8.css';
 import './v1.9.css';
 import './v2.0.css';
 import './v2.1.css';
+import './v2.2.css';
 import AppV14 from './AppV14';
 
 createRoot(document.getElementById('root')!).render(
